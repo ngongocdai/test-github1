0c0b72d4ab8ed766b77f5a8273c590a5479f0b6a
ngongocdai <115610459+ngongocdai@users.noreply.github.com>
2022-10-14 08:38:37 +0700

    test

---
+++ tes tc3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a;
+	scanf("%d", &a);
+	if(a>0){
+	    printf("a la so duong");
+	}else{
+		printf("a khong phai la so duong");
+	}
+}
---
+++ test c2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int maina(){
+	int a;
+	scanf("%d", &a);
+	
+	int b;
+	scanf("%d", &b);
+	
+	int c;
+	c = a+b;
+	
+	printf("c = %d",c) ;
+
+}
